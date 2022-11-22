8413bcd9c2e62833b93d6d8b9562b031ce06e764
Josal <joyalmeida321@gmail.com>
2022-11-22 14:55:45 +0530

    added indendation

--- 22-Nov crowdfunding/program.sol
+++ 22-Nov crowdfunding/program.sol
@@ -11,21 +11,21 @@ contract crowdfunding{
 	uint public noOfContributors;
 
 	constructor(uint _target, uint _deadline){
-        target = _target;
-        deadline = block.timestamp + _deadline;
-        minimumContribution = 100 wei;
-        manager = msg.sender;
+		target = _target;
+		deadline = block.timestamp + _deadline;
+		minimumContribution = 100 wei;
+		manager = msg.sender;
 	}
 
 	function sendEth() public payable{
 		require(manager != msg.sender, "Manager cannot contribute");
-        require(block.timestamp < deadline, "Deadline has passed");
-        require(msg.value >= minimumContribution, "Minimum Contribution is not met");
+		require(block.timestamp < deadline, "Deadline has passed");
+		require(msg.value >= minimumContribution, "Minimum Contribution is not met");
 
-        if(contributors[msg.sender] == 0){
-            noOfContributors++;
-        }
-        contributors[msg.sender]+=msg.value;
-        raisedAmount+=msg.value;
+		if(contributors[msg.sender] == 0){
+			noOfContributors++;
+		}
+		contributors[msg.sender]+=msg.value;
+		raisedAmount+=msg.value;
 	}
 }
