c8c3ae132d8f693f68e2174bbdf3a271ef3d97f2
Josal <joyalmeida321@gmail.com>
2022-11-22 15:33:17 +0530

    added vote Request and makePayment

--- 22-Nov crowdfunding/program.sol
+++ 22-Nov crowdfunding/program.sol
@@ -65,4 +65,21 @@ contract crowdfunding{
 		newRequest.completed = false;
 		newRequest.noOfVoters = 0;
 	}
+
+	function voteRequest(uint _requestNo) public {
+		require(contributors[msg.sender] > 0, "You must be contributor");
+		Request storage thisRequest = requests[_requestNo];
+		require(thisRequest.voters[msg.sender] == false, "You have already voted");
+		thisRequest.voters[msg.sender] = true;
+		thisRequest.noOfVoters++;
+	}
+
+	function makePayment(uint _requestNo) public {
+		require(raisedAmount >= target);
+		Request storage thisRequest = requests[_requestNo];
+		require(thisRequest.completed == false, "The request has been completed");
+		require(thisRequest.noOfVoters > noOfContributors/2, "Majoruty does not support");
+		thisRequest.recipient.transfer(thisRequest.value);
+		thisRequest.completed = true;
+	}
 }
